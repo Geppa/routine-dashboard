5eb41a385a9f82568963275afc3bf43864dff235
Geppa ( Minkuk Kim ) <asdjklfgh97@naver.com>
2025-08-12 04:56:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,5 @@
-const CACHE = 'routine-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./')))
-  );
-});
+const CACHE = 'routine-v4';
+const ASSETS = ['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install', e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
+self.addEventListener('activate', e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./'))));});
